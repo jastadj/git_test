b891224084385c819768a7fb28e86359df681b49
John J. <jastadj@gmail.com>
2015-01-20 14:36:32 -0800

    add two numbers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <cstdlib>
+#include <iostream>
+
+int addTwoNumbers(int a, int b)
+{
+    return a + b;
+}
 
 int main(int argc, char *argv[])
 {
 
+    int a = 5;
+    int b = 2;
+
+    std::cout << "Adding " << a << " to " << b << " = " << addTwoNumbers(a,b) << std::endl;
 
     return 0;
 }
