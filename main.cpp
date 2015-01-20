720a04428b3d99303dd748c7e9f93e3bdb3e7d23
John J. <jastadj@gmail.com>
2015-01-20 14:43:50 -0800

    change1

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 
     int a = 5;
-    int b = 2;
+    int b = 9;
 
     std::cout << "Adding " << a << " to " << b << " = " << addTwoNumbers(a,b) << std::endl;
 
